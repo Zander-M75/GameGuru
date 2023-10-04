358b029320f41d8ea2e98476e959bff138c9aff1
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-10-04 15:43:39 -0400

    light and dark mode context added

--- ReactGames/src/App.jsx
+++ ReactGames/src/App.jsx
@@ -4,16 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Pages/Home'
 import Header from './Components/Header'
+import { ThemeContext } from './Context/ThemeContext'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState("dark")
 
   return (
     <>
-      <div className='app'>
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <div className={`${theme} 
+      ${theme == 'dark' ? 'bg-[#121212]' : null} h-[100vh]`}>
         <Header />
         <Home />
         </div>
+        </ThemeContext.Provider>
     </>
   )
 }
--- ReactGames/src/Components/Header.jsx
+++ ReactGames/src/Components/Header.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react';
+import logo from "./../assets/images/logo.jpg";
+import { HiSun, HiMoon, HiOutlineMagnifyingGlass } from "react-icons/hi2";
+import { ThemeContext } from '../Context/ThemeContext';
 
 function Header() {
+    const [toggle, setToggle] = useState(true)
+    const {theme, setTheme} = useContext(ThemeContext)
+
+    useEffect(() => {
+        console.log("Theme", theme)
+    }, [])
+
+
   return (
-    <div>Header</div>
+    <div className='flex items-center p-3'>
+        <img src={logo} width={80} height={80} className='rounded-full' />
+        <div className='flex bg-slate-200 p-2 w-full mx-5 rounded-full items-center'>
+            <HiOutlineMagnifyingGlass />
+            <input 
+            type='text' 
+            placeholder='Search Games' 
+            className='px-2 bg-transparent dark:bg-cyan-500 outline-none' 
+            />
+        </div>
+        <div>
+            {theme =='light' ? 
+            <HiMoon 
+            className='text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer' 
+            onClick={() => setTheme('dark')} /> : 
+            <HiSun 
+            className='text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer' 
+            onClick={() => setTheme('light')} />}
+        </div>
+    </div>
   )
 }
 
