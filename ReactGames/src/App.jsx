935115ea334ca313d6491ddbb727bbaa4acf9e55
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-10-04 13:09:35 -0400

    folder structure implemented

--- ReactGames/src/App.jsx
+++ ReactGames/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Pages/Home'
+import Header from './Components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='text-2xl text-blue-700'>
-        Hello
+      <div className='app'>
+        <Header />
+        <Home />
         </div>
     </>
   )
