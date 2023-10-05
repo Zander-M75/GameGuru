e0267a8d2a088264c16a5b1a2c8df57d745c663b
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-10-05 16:31:38 -0400

    side bar fetch and styling complete

--- ReactGames/src/Pages/Home.jsx
+++ ReactGames/src/Pages/Home.jsx
@@ -3,8 +3,8 @@ import GenreList from '../Components/GenreList'
 
 function Home() {
   return (
-    <div className='grid grid-cols-4'>
-        <div className='bg-red-500 h-full hidden md:block'>
+    <div className='grid grid-cols-4 px-8'>
+        <div className='h-full hidden md:block'>
             <GenreList />
         </div>
         <div className='col-span-4 md:col-span-3 bg-blue-500'>Game List</div>
