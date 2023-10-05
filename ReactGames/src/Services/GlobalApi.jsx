7a7b9b46862ae7f2a28b330c012b20b26fc3bfd7
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-10-05 17:03:31 -0400

    banner component and styled implemented

--- ReactGames/src/Services/GlobalApi.jsx
+++ ReactGames/src/Services/GlobalApi.jsx
@@ -8,9 +8,11 @@ const axiosCreate = axios.create({
 });
 
 const getGenreList = axiosCreate.get('/genres?key='+key)
+const getAllGames = axiosCreate.get('/games?key='+key)
 
 export default {
-    getGenreList
+    getGenreList,
+    getAllGames
 };
 
 
